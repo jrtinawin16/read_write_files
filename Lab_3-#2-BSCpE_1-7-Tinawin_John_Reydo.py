152f0b0f5b450e861d6dec30bd484f45c7ef1836
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-04-06 20:52:32 +0800

    Accomplished conditions

--- Lab_3-#2-BSCpE_1-7-Tinawin_John_Reydo.py
+++ Lab_3-#2-BSCpE_1-7-Tinawin_John_Reydo.py
@@ -2,4 +2,10 @@ with open("integers.txt", "r") as source_file:
     lines = source_file.readlines()
 
 with open("double.txt", "w") as double_file, open("triple.txt", "w") as triple_file:
-    
+    for line in lines:
+        for value in line.split():
+            number = int(value)
+            if number % 2 == 0:
+                double_file.write(str(number ** 2) + "\n")
+            else:
+                triple_file.write(str(number ** 3) + "\n")

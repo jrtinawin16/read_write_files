b07e308c6eb2507bca52b83de8722b0f98947cdc
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-04-06 21:26:30 +0800

    Added conditions and prints output

--- Lab_3-#3-BSCpE_1-7-Tinawin_John_Reydo.py
+++ Lab_3-#3-BSCpE_1-7-Tinawin_John_Reydo.py
@@ -3,5 +3,11 @@ with open ("gwa.txt", "r") as gwa_file:
     highest_gwa = float("inf")
 
     for line in gwa_file.readlines():
-        name, gwa = line.strip().split(",")
-        
+        student_name, student_gwa = line.strip().split(",")
+        student_gwa = float(student_gwa)
+        if student_gwa < highest_gwa:
+            highest_gwa = student_gwa
+            highest_name = student_name
+
+print("Student with Highest GWA: ", highest_name)
+print("GWA: ", highest_gwa)
